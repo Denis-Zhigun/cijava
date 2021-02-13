cc908d28a495a9e892919a6c84d6983627b297b2
Denis Zhigun <zhigundv@gmail.com>
2021-02-13 17:05:05 +0300

    Added test findMaxAmongBigger

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -7,14 +7,16 @@ import static org.junit.jupiter.api.Assertions.*;
 class StatisticsServiceTest {
 
   @Test
-  void findMax() {
+  void findMaxAmongSmaller() {
     StatisticsService service = new StatisticsService();
+    long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12, 7, 6, 1};
+    assertEquals(12, service.findMax(incomesInBillions));
+  }
 
-    long[] incomesInBillions = {12,5, 8, 4, 5, 3, 8, 6, 11, 11, 12,7,6,1};
-    long expected = 12;
-
-    long actual = service.findMax(incomesInBillions);
-
-    assertEquals(expected, actual);
+    @Test
+    void findMaxAmongBigger() {
+      StatisticsService service = new StatisticsService();
+      long[] incomesInBillions = {10, 450, 2000, 0, 2};
+      assertEquals(2000, service.findMax(incomesInBillions));
   }
 }
